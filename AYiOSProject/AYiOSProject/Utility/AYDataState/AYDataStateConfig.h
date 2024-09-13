6aca10f6459d6287f3464eb91ae7b16b4378b701
alpha yu <wtuyp@163.com>
2024-09-13 15:05:48 +0800

    AYDataState 更新

--- AYiOSProject/AYiOSProject/Utility/AYDataState/AYDataStateConfig.h
+++ AYiOSProject/AYiOSProject/Utility/AYDataState/AYDataStateConfig.h
@@ -9,38 +9,38 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-/// 数据状态视图配置
+/// 视图数据状态配置
 @interface AYDataStateConfig : NSObject
 
 // 图片
-@property (nonatomic, strong, nullable) UIImage *image; ///< 与 imageName 二选一，比 imageName 优先级高
+@property (nonatomic, strong, nullable) UIImage *image;     ///< 图片。与 imageName 二选一，比 imageName 优先级高
 @property (nonatomic, strong, nullable) NSString *imageName;
-@property (nonatomic, assign) CGSize imageSize;         ///< 图片大小
+@property (nonatomic, assign) CGSize imageSize;             ///< 图片大小
 
 // 标题（单行）
 @property (nonatomic, copy, nullable) NSString *title;
-@property (nonatomic, strong) UIColor *titleColor;
-@property (nonatomic, strong) UIFont *titleFont;
-@property (nonatomic, assign) CGFloat titleTopGap;  /// 与上一项的垂直距离，没有上一项将忽略该值
+@property (nonatomic, strong, nullable) UIColor *titleColor;
+@property (nonatomic, strong, nullable) UIFont *titleFont;
+@property (nonatomic, assign) CGFloat titleTopGap;          ///< 与图片的垂直距离
 
 // 详情（多行）
 @property (nonatomic, copy, nullable) NSString *detail;
-@property (nonatomic, strong) UIColor *detailColor;
-@property (nonatomic, strong) UIFont *detailFont;
-@property (nonatomic, assign) CGFloat detailWidth;  // 因为多行，要有一个宽度。没设置则用父视图的宽度
-@property (nonatomic, assign) CGFloat detailTopGap;
+@property (nonatomic, strong, nullable) UIColor *detailColor;
+@property (nonatomic, strong, nullable) UIFont *detailFont;
+@property (nonatomic, assign) CGFloat detailWidth;          ///< 详情宽度。因为多行，要有一个宽度。没设置则用父视图的宽度
+@property (nonatomic, assign) CGFloat detailTopGap;         ///< 与标题的垂直距离
 
 // 按键
 @property (nonatomic, copy, nullable) NSString *btnTitle;
-@property (nonatomic, strong) UIColor *btnTitleColor;
-@property (nonatomic, strong) UIFont *btnTitleFont;
-@property (nonatomic, strong) UIColor *btnBackgroundColor;
-@property (nonatomic, assign) CGFloat btnBorderWidth;
-@property (nonatomic, strong) UIColor *btnBorderColor;
+@property (nonatomic, strong, nullable) UIColor *btnTitleColor;
+@property (nonatomic, strong, nullable) UIFont *btnTitleFont;
+@property (nonatomic, strong, nullable) UIColor *btnBackgroundColor;
 @property (nonatomic, assign) CGSize btnSize;
 @property (nonatomic, assign) CGFloat btnCornerRadius;
-@property (nonatomic, assign) CGFloat btnTopGap;
+@property (nonatomic, assign) CGFloat btnBorderWidth;
+@property (nonatomic, strong, nullable) UIColor *btnBorderColor;
 @property (nonatomic, copy, nullable) void (^btnActionBlock)(void);
+@property (nonatomic, assign) CGFloat btnTopGap;            ///< 与详情的垂直距离
 
 @end
 
